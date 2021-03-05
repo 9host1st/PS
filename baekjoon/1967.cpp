a2659d8056ea4d467a48a3ecbcdb96e4b7e16c59
9host1st <9host1st@gmail.com>
2021-03-05 14:41:06 +0900

    PS

--- baekjoon/1967.cpp
+++ baekjoon/1967.cpp
@@ -5,15 +5,34 @@ vector<pii> a[10001];
 int dist[10001];
 bool vis[10001];
 int x, y, d, n;
-
-int dfs(int k) {
+int ans;
+void init() { for(int i = 1; i <= n; i++) vis[i] = false; }
+void dfs(int k, int ret) {
+    vis[k] = true;
+    ans = max(ans, ret);
+    if(k == n) {
+        ans = max(ans, ret);
+        return;
+    }
+    for(int i = 0; i < a[k].size(); i++) {
+        if(a[k][i].first && !vis[a[k][i].first]) {
+            dfs(a[k][i].first, ret + a[k][i].second);
+        }
+    }
 }
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> n;
-    while(n--) {
+    for(int i = 0; i < n - 1; i++) {
         cin >> x >> y >> d;
         a[x].push_back({y, d});
         a[y].push_back({x, d});
     }
+    for(int i = 1; i <= n; i++) {
+        init();
+        dfs(i, 0);
+    }
+    cout << ans << '\n';
 }
